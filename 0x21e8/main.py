3a886fa948c5c654ac0636894d316be2f0bac583
Firat Berk Cakar <firat@riddleandcode.com>
2022-07-08 16:00:19 +0200

    initial planetmint working version

--- 0x21e8/main.py
+++ 0x21e8/main.py
@@ -1,21 +1,25 @@
 from typing import Optional
 
+from cryptoconditions import Ed25519Sha256
+from cryptoconditions.crypto import Ed25519SigningKey
 from fastapi import FastAPI
+from nacl.encoding import HexEncoder
+
 from model import IssuingRequest, TokenRelatedAccounts, accounts_to_json
 from notarize import get_asset_description
 
 import binascii
 from planetmint_driver import Planetmint
 from planetmint_driver.offchain import fulfill_with_signing_delegation
-#from ipld import marshal, multihash
+# from ipld import marshal, multihash
 from wallet.liquid import get_liquid_keys, register_asset_id, register_asset_id_on_liquid
 from wallet.issue2liquid import issue_tokens
-from wallet.planetmint import get_planetmint_keys, get_planetmint_keys_tc
+from wallet.planetmint import get_planetmint_keys, get_planetmint_keys_tc, get_seed
 
 app = FastAPI()
 
-#plntmnt = Planetmint('http://lab.r3c.network:9984')    
-plntmnt = Planetmint('https://test.ipdb.io')    
+# plntmnt = Planetmint('http://lab.r3c.network:9984')
+plntmnt = Planetmint('https://test.ipdb.io')
 MNEMONIC_PHRASE = "supreme layer police brand month october rather rack proud strike receive joy limit random hill inside brand depend giant success quarter brain butter mechanic"
 
 
@@ -23,56 +27,82 @@ MNEMONIC_PHRASE = "supreme layer police brand month october rather rack proud st
 async def root():
     return {"message"}
 
+
 @app.get("/keypairs")
-async def getkeypairs():    
+async def getkeypairs():
     accounts = TokenRelatedAccounts
-    accounts.plpriv , accounts.plpub = get_planetmint_keys( MNEMONIC_PHRASE )
+    accounts.plpriv, accounts.plpub = get_planetmint_keys(MNEMONIC_PHRASE)
     accounts.plpriv = accounts.plpriv.decode()
     accounts.plpub = accounts.plpub.decode()
-    accounts.lqpriv, accounts.lqpub = get_liquid_keys( MNEMONIC_PHRASE )
-    return accounts_to_json( accounts )    
+    accounts.lqpriv, accounts.lqpub = get_liquid_keys(MNEMONIC_PHRASE)
+    return accounts_to_json(accounts)
+
+
+@app.get("/post_planet_mint_tx")
+async def run():
+    import base58
+    seed_bytes = get_seed()
+    pl_sk, pl_vk = get_planetmint_keys_tc(seed_bytes)
+
+    def pl_signing_function(input, message):
+        from nacl.signing import SigningKey
+        ncal_sk = SigningKey(pl_sk)
+        res = ncal_sk.sign(message)
+        return res.signature  # signature matches signature from other schemes
+
+    tx = plntmnt.transactions.prepare(
+        operation='CREATE',
+        signers=[base58.b58encode(pl_vk).decode()],
+        asset={'data': {'Issued Token': "test"}})
+
+    signed_tx = fulfill_with_signing_delegation(tx, pl_signing_function)
+    normal_fulfill_with_private_key = plntmnt.transactions.fulfill(tx, private_keys=[base58.b58encode(pl_sk)])
+    token_nft = plntmnt.transactions.send_commit(signed_tx)
+
+    #print(token_nft)
+
 
 @app.post("/issuetokens")
 async def issuetokens(issueTokens: IssuingRequest):
     # get wallet addresses (issuer, private & pub for )
-    pl_sk, pl_vk = get_planetmint_keys_tc( MNEMONIC_PHRASE )
-    lq_sk, lq_vk = get_liquid_keys( MNEMONIC_PHRASE )
+    pl_sk, pl_vk = get_planetmint_keys_tc(MNEMONIC_PHRASE)
+    lq_sk, lq_vk = get_liquid_keys(MNEMONIC_PHRASE)
 
     def pl_signing_function(tx, message):
         import base58
         from nacl.signing import SigningKey
 
-        sk, vk = get_planetmint_keys_tc( MNEMONIC_PHRASE )
-        #print(sk.encode(encoding='base58'))
-        ncal_sk = SigningKey( base58.b58decode(sk) )
+        sk, vk = get_planetmint_keys_tc(MNEMONIC_PHRASE)
+        # print(sk.encode(encoding='base58'))
+        ncal_sk = SigningKey(base58.b58decode(sk))
         res = ncal_sk.sign(message).signature
         return res
 
-    asset_insurance = { "insurance_contract": "aösjfaölkdjfaoijdfäpowkeßf0iküpfokasüdfpwokfüisajfüoasjfopiajdfüoja" } # random string representing a asset insuring contract
-#    marshalled = marshal( asset_insurance )
-#    hashed_marshalled = multihash(marshalled)
+    asset_insurance = {
+        "insurance_contract": "aösjfaölkdjfaoijdfäpowkeßf0iküpfokasüdfpwokfüisajfüoasjfopiajdfüoja"}  # random string representing a asset insuring contract
+    #    marshalled = marshal( asset_insurance )
+    #    hashed_marshalled = multihash(marshalled)
     hashed_marshalled = "test ipld"
 
     # create the token NFT - e.g. the token notarization on planetmint
-    nft_asset=get_asset_description( issueTokens, lq_vk, pl_vk.decode(), hashed_marshalled)
+    nft_asset = get_asset_description(issueTokens, lq_vk, pl_vk.decode(), hashed_marshalled)
     print(nft_asset)
     tx = plntmnt.transactions.prepare(
         operation='CREATE',
         signers=pl_vk.decode(),
-        asset= {'data': { 'Issued Token' : nft_asset}})
-    signed_tx = fulfill_with_signing_delegation( tx,
+        asset={'data': {'Issued Token': nft_asset}})
+    signed_tx = fulfill_with_signing_delegation(tx,
                                                 pl_signing_function)
 
     token_nft = plntmnt.transactions.send_commit(signed_tx)
-    
 
     # issue tokens
-    asset_id = issue_tokens( issueTokens, lq_vk, token_nft['id'], hashed_marshalled)
+    asset_id = issue_tokens(issueTokens, lq_vk, token_nft['id'], hashed_marshalled)
 
     # register assets on local node
-    register_asset_id( asset_id )
-    
-    #register assests on liquid
-    #register_asset_id_on_liquid( asset_id )
-        
-    return { "token NFT": token_nft['id'], "signed tx" : signed_tx, "ipdl": hashed_marshalled, "asset_id": asset_id }
+    register_asset_id(asset_id)
+
+    # register assests on liquid
+    # register_asset_id_on_liquid( asset_id )
+
+    return {"token NFT": token_nft['id'], "signed tx": signed_tx, "ipdl": hashed_marshalled, "asset_id": asset_id}
--- 0x21e8/wallet/planetmint.py
+++ 0x21e8/wallet/planetmint.py
@@ -1,56 +1,33 @@
-from cryptoconditions.crypto import Ed25519SigningKey
 from mnemonic import Mnemonic
-from wallet.keymanagement import seed_to_extended_key
-import binascii
 import TrezorCrypto
 
+HARDENED = 0x80000000
+PLANET_VERSION_PUBLIC = 0x02d41400
+PLANET_VERSION_PRIVATE = 0x02d40fc0
 
 
+def get_seed() -> bytes:
+    mnemonic_obj = Mnemonic("english")
+    phrase = mnemonic_obj.generate(256)
+    print("New mnemonic phrase: ", phrase)
+    seed_bytes = mnemonic_obj.to_seed(phrase, '0x21e8')
+    print(f'Seed {seed_bytes.hex()}')
+    return seed_bytes
 
 
-def get_planetmint_keys_tc( mnemonic_phrase ):
-    PLANET_VERSION_PUBLIC = 0x02d41400
-    PLANET_VERSION_PRIVATE = 0x02d40fc0
-    MNEMONIC_PHRASE = 'supreme layer police brand month october rather rack proud strike receive joy limit random hill inside brand depend giant success quarter brain butter mechanic'
-    
-    if not mnemonic_phrase : mnemonic_phrase = MNEMONIC_PHRASE
-    seed = Mnemonic.to_seed( mnemonic_phrase, '0x21e8' )
-    print (f' seed { seed }')
-    print (f' seed { binascii.hexlify( seed ) }')
+def get_planetmint_keys_tc(seed: bytes):
 
-    node = TrezorCrypto.HDNode( )
-    node.from_seed(seed, 16)
-    node = node.private_ckd(0x80000000)
-    sk_raw = node.private_key_raw()
-    print( f"raw private key : { binascii.hexlify(sk_raw)}")
-    b = TrezorCrypto.b58()
-    pl_sk = b.b58_encode_check(sk_raw, 20, 0)
-    print( f"Planetmint address private key: { pl_sk }")
-    print( f"Planetmint chaincode: { binascii.hexlify(node.chain_code()) }")
-    print( f"planetmint address key: { node.get_address( PLANET_VERSION_PUBLIC )}")
-    print( f"planetmint raw public key: { binascii.hexlify(node.fill_public_key())}")
-    fingerprint = 0
-    
-    print(f"Planetmint private key : { node.xprv( fingerprint, PLANET_VERSION_PRIVATE)}")
-    print(f"Planetmint public key : { node.xpub( fingerprint, PLANET_VERSION_PUBLIC) }")
-    return pl_sk, node.get_address( PLANET_VERSION_PUBLIC )
+    node = TrezorCrypto.from_seed(seed, TrezorCrypto.ED25519_NAME)
+    # [Chain m/0'] check tests/bip32_tests.py for more derivation path examples
+    node.derive(HARDENED | 0)
+    print(f"raw private key : {(node.private_key().hex())}")
+    print(f"Planetmint chaincode: {node.chain_code().hex()}")
+    print(f"planetmint address public: {node.address(PLANET_VERSION_PUBLIC)}")
+    print(f"planetmint raw public key: {node.public_key().hex()}")
 
-
-def get_planetmint_keys( mnemonic_phrase ):
-    seed = Mnemonic.to_seed( mnemonic_phrase, '0x21e8' )
-    ext_seed = seed_to_extended_key(seed)
-    sk_raw = Ed25519SigningKey.generate_with_seed(ext_seed.privkey)
-    vk = sk_raw.get_verifying_key().encode(encoding='base58')
-    sk = sk_raw.encode(encoding='base58')
-    return sk, vk
-    
-    
-#Planetmint raw private key: 8249232f45b8231e6765a9c2c180aac69c54a261fd618c97f84e464af871f -> done!
-#Planetmint address private key: k4DYEbRbYBbehV2Qnq9vRD75PCWTkUTR
-#Planetmint chaincode: 7fb5fb5fd8d7da70f62e33a3d592a4885bff857e3b23379a22c98bb8b9e2a0
-#Planetmint address key: 2gs2o1dKRSFnPztDGPHHkWqwqixUCqjdT1vrZt
-#Planetmint raw public key: 114715fbcea4e14ed6d24296ea6e11333372b8f12cb5bffdd21f3a327b2b24e
-#Planetmint private key: bprv35Rc2dqCQhrCE9zjscZvJFeBfD93jbJLwDqgyPPwUHWC83neHVh4wfxceHyFu2WLHTdGhDpsKF9JXbRoS2TFYjcsZsN9pwRo9a3Bsk6A5CK
-#Planetmint public key: bpubq8d4saQC76AjUXo9hNtSu1sCKD4CgGpXYvpNgZbgCEdekyfXLA3MHe5QSgeUU5WzYa5pTHWv2GMZzDpWF9FAM7ZBBUGuSpD4R9dFZMcFXvH
-#MNEMONIC_PHRASE = 'supreme layer police brand month october rather rack proud strike receive joy limit random hill inside brand depend giant success quarter brain butter mechanic'
-#get_planetmint_keys( MNEMONIC_PHRASE)
+    print(f"Planetmint private key : {node.serialize_private(PLANET_VERSION_PRIVATE)}")
+    print(f"Planetmint public key : {node.serialize_public(PLANET_VERSION_PUBLIC)}")
+    signing_key = node.private_key()
+    public_key = node.public_key()
+    # don't forget stripping the first byte of the public key (01 depicts edwards)
+    return signing_key, public_key[1:]
