f6f30307707fb22c2acec885739f7401243c0a04
Jürgen Eckel <juergen@riddleandcode.com>
2022-10-21 17:32:01 +0200

    deployment via poetry
    Signed-off-by: Jürgen Eckel <juergen@riddleandcode.com>

--- install-service.sh
+++ install-service.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 sudo cp 0x21e8.service /etc/systemd/system
 sudo systemctl daemon-reload
 sudo systemctl enable 0x21e8.service
--- install.sh
+++ install.sh
@@ -6,10 +6,13 @@ sudo apt install autotools
 sudo apt install autogen
 sudo apt install libtool
 sudo apt install swig
+sudo apt install libffi-dev
+sudo apt install libssl-dev
+sudo apt install make swig meson cmake zsh gcc
 python -m pip install --upgrade pip
 pip install --upgrade pipenv
+pip install --upgrade poetry
 
-pipenv run pip install pysha3~=1.0.2 python-rapidjson>=1.0.0 python-rapidjson-schema==0.1.1 base58 pynacl==1.4.0 pyasn1 cryptography==3.4.7 requests>=2.20.0 planetmint-cryptoconditions>=0.10.0 TrezorCryptoTestRc planetmint-ipld wallycore==0.8.2 mnemonic fastapi uvicorn bitcoin-utils
-pipenv run pip install planetmint-driver==0.11.1 --no-deps
-pipenv run pip install ./external_packages_armv7/zenroom-2.1.0.dev1662451250-cp39-cp39-linux_armv7l.whl
 
+poetry add  ./external_packages_armv7/zenroom-2.1.0.dev1655293214-cp39-cp39-linux_armv7l.whl
+poetry install
