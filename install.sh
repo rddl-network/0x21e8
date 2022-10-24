69e1ae1107ca8c3e80c24c0be0bec888af069dd3
Jürgen Eckel <juergen@riddleandcode.com>
2022-10-24 22:22:28 +0200

    fixed deplyoment issue with virtualenv and pip
    Signed-off-by: Jürgen Eckel <juergen@riddleandcode.com>

--- install.sh
+++ install.sh
@@ -11,7 +11,8 @@ sudo apt -y install libffi-dev
 sudo apt -y install libssl-dev
 sudo apt -y install make swig meson cmake zsh gcc
 curl https://sh.rustup.rs -sSf | sh
-python -m pip install --upgrade pip
-pip install --upgrade poetry
+#python -m pip install --upgrade pip
+pip install poetry
+pip uninstall virtualenv
 source ~/.profile && source "$HOME/.cargo/env" && poetry add  ./external_packages_armv7/zenroom-2.1.0.dev1655293214-cp39-cp39-linux_armv7l.whl
 source ~/.profile && poetry install
