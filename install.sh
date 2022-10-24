a208a821c386497413b28246f2f3dfbedd1f433f
Jürgen Eckel <juergen@riddleandcode.com>
2022-10-24 16:44:49 +0200

    added rust removed pipenv
    Signed-off-by: Jürgen Eckel <juergen@riddleandcode.com>

--- install.sh
+++ install.sh
@@ -2,17 +2,14 @@
 
 sudo apt install python3-pip
 sudo apt install autoconf
-sudo apt install autotools
 sudo apt install autogen
 sudo apt install libtool
 sudo apt install swig
 sudo apt install libffi-dev
 sudo apt install libssl-dev
 sudo apt install make swig meson cmake zsh gcc
+curl https://sh.rustup.rs -sSf | sh
 python -m pip install --upgrade pip
-pip install --upgrade pipenv
 pip install --upgrade poetry
-
-
-poetry add  ./external_packages_armv7/zenroom-2.1.0.dev1655293214-cp39-cp39-linux_armv7l.whl
-poetry install
+source ~/.profile && source "$HOME/.cargo/env" && poetry add  ./external_packages_armv7/zenroom-2.1.0.dev1655293214-cp39-cp39-linux_armv7l.whl
+source ~/.profile && poetry install
