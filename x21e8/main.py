a429a9a2804cf56bf3b68d9cfa43f6235630f993
Jürgen Eckel <juergen@riddleandcode.com>
2022-11-28 09:05:59 +0100

    fix: returning proper json objects
    Signed-off-by: Jürgen Eckel <juergen@riddleandcode.com>

--- x21e8/main.py
+++ x21e8/main.py
@@ -41,7 +41,7 @@ app = FastAPI(openapi_tags=tags_metadata)
 async def set_data(in_data_dict: dict, encrypt: bool = False):
     try:
         cid = store_asset(in_data_dict, encrypt_data=encrypt)
-        return cid
+        return { "cid": cid }
     except FileNotFoundError:
         raise HTTPException(
             status_code=421,
@@ -62,10 +62,11 @@ async def get_data(cid: str, link2data: bool = False, decrypt: bool = False):
                 detail=f"The requested URL could not be resolved: { e.code } : { e.reason }.",
             )
 
-    return data
+    return { "cid": data }
 
 
-@app.get("/cid", tags=["Assets"])
+@app.get("/cid", tags=["Assets"], deprecated=True)
+@app.get("/assets", tags=["Assets"])
 async def get_cid_token(cid_token):
     try:
         nft_tx, nft_cid = resolve_asset_token(cid_token)
@@ -83,7 +84,8 @@ async def get_cid_token(cid_token):
         )
 
 
-@app.post("/cid", tags=["Assets"])
+@app.post("/cid", tags=["Assets"], deprecated=True)
+@app.post("/assets", tags=["Assets"])
 async def set_cid_token(cid: str):
     # get wallet addresses (issuer, private & pub for )
     try:
@@ -186,7 +188,7 @@ async def create_seed_and_provision(number_of_words: int):
         strength = 128
     else:
         raise HTTPException(status_code=420, detail="A mnemonic has to contain 12 or 24 words")
-    return create_and_save_seed(strength)
+    return { 'mnemonic': create_and_save_seed(strength) }
 
 
 @app.post("/seed", tags=["Seed"])
@@ -203,7 +205,7 @@ async def recover_seed_from_mnemonic(mnemonic_phrase: str):
 async def get_multihash(json_data: dict, encrypt: bool = False):
     try:
         hashed_marshalled = multihashed(json_data, encrypt)
-        return hashed_marshalled
+        return {'cid' : hashed_marshalled }
     except FileNotFoundError:
         raise HTTPException(
             status_code=421,
