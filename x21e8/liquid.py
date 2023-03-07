9f539cb4de0eea13062f46bc9e713271809b2f80
Jürgen Eckel <juergen@riddleandcode.com>
2023-03-07 16:50:45 +0100

    * structured and simplified code * added new env variables
    Signed-off-by: Jürgen Eckel <juergen@riddleandcode.com>

--- x21e8/liquid.py
+++ x21e8/liquid.py
@@ -4,60 +4,65 @@ import six
 from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
 
 from x21e8.model import IssuingRequest
-from x21e8.config import LQD_RPC_ENDPOINT, build_liquid_auth_proxy_url
+from x21e8.config import build_liquid_auth_proxy_url
 
 TOKEN_AMOUNT = 1
 VERSION = 0
 FEE_RATE = 0.03000000
 
 
-def issue_tokens(issue_request: IssuingRequest, nft_token, ipdl):
-    name = issue_request.name
-    ticker = issue_request.ticker
-    asset_amount = issue_request.amount
-    precision = issue_request.precision
-
+def get_keys(rpc_connection: AuthServiceProxy):
     pubkey = None
     asset_addr = None
     token_addr = None
-    rpc_connection = None
     try:
-        rpc_connection = AuthServiceProxy(
-            build_liquid_auth_proxy_url(),
-        )
-        new_addr = rpc_connection.getnewaddress("riddlemint", "legacy")
+        new_addr = rpc_connection.getnewaddress("riddlemint", "legacy")  # args: label, address type
         validate_addr = rpc_connection.getaddressinfo(new_addr)
         pubkey = validate_addr["pubkey"]
         asset_addr = new_addr
-        new_addr = rpc_connection.getnewaddress("MoteMagic", "legacy")
+        new_addr = rpc_connection.getnewaddress("MoteMagic", "legacy")  # args: label, address type
         token_addr = new_addr
-
     except JSONRPCException as json_exception:
         print("A JSON RPX exception occured: " + str(json_exception))
     except Exception as general_exception:
         print("An exception occured: " + str(general_exception))
-
+    
     print(pubkey)
     print(asset_addr)
     print(token_addr)
+    
+    return (pubkey, asset_addr, token_addr)
 
-    contract = f'{{"entity":{{"domain":"{LQD_RPC_ENDPOINT}"}}, "issuer_pubkey":"{pubkey}", "nft":{{"token":"{nft_token}", "ipld":"{ipdl}"}}, "name":"{name}", "precision":{precision}, "ticker":"{ticker}", "version":{VERSION}}}'
-    rpc_connection.settxfee(FEE_RATE)
-    print(contract)
+
+def create_contract(issue_request: IssuingRequest, nft_token:str, cid:str, pubkey:str):
+    
+    contract = f'{{"entity":{{"domain":"{issue_request.public_url}"}}, "issuer_pubkey":"{pubkey}", "nft":{{"token":"{nft_token}", "ipld":"{cid}"}}, "name":"{issue_request.name}", "precision":{issue_request.precision}, "ticker":"{issue_request.ticker}", "version":{VERSION}}}'
     contract_sorted = json.dumps(json.loads(contract), sort_keys=True, separators=(",", ":"))
     contract_hash = hashlib.sha256(six.ensure_binary(contract_sorted)).hexdigest()
-    print(contract_hash)
     contract_hash_rev = "".join(reversed([contract_hash[i : i + 2] for i in range(0, len(contract_hash), 2)]))
+    print(contract)
+    print(contract_hash)
     print(contract_hash_rev)
+    return (contract, contract_hash_rev)
 
+
+def issue_tokens(issue_request: IssuingRequest, nft_token:str, cid:str):
+
+    rpc_connection = AuthServiceProxy(
+        build_liquid_auth_proxy_url(),
+    )
+
+    (pubkey, asset_addr, token_addr) = get_keys(rpc_connection=rpc_connection)
+
+    (contract, contract_hash_rev) = create_contract(issue_request, nft_token, cid, pubkey)
+
+    rpc_connection.settxfee(FEE_RATE)
     raw_tx = rpc_connection.createrawtransaction([], [{"data": "00"}])
-
     print(raw_tx)
 
     # get funded raw transaction
     frt = rpc_connection.fundrawtransaction(raw_tx, {"feeRate": FEE_RATE})
     print(frt)
-
     hex_frt = frt["hex"]
     print(hex_frt)
 
@@ -65,7 +70,7 @@ def issue_tokens(issue_request: IssuingRequest, nft_token, ipdl):
         hex_frt,
         [
             {
-                "asset_amount": asset_amount,
+                "asset_amount": issue_request.amount,
                 "asset_address": asset_addr,
                 "token_amount": TOKEN_AMOUNT,
                 "token_address": token_addr,
