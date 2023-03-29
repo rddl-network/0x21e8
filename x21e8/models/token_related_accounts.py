2a75ac1f875c767d6157421bcc55433ed284f7b9
Jürgen Eckel <juergen@riddleandcode.com>
2023-03-29 09:20:50 +0200

    blackified
    Signed-off-by: Jürgen Eckel <juergen@riddleandcode.com>

--- x21e8/models/token_related_accounts.py
+++ x21e8/models/token_related_accounts.py
@@ -6,11 +6,9 @@ class TokenRelatedAccounts(BaseModel):
     lq_pub: str
     pl_priv: str
     pl_pub: str
-    
+
     def accounts_to_json(self) -> dict:
         return {
             "Liquid": {"private": self.lq_priv, "public": self.lq_pub},
             "Planetmint": {"private": self.pl_priv, "public": self.pl_pub},
         }
-
-
