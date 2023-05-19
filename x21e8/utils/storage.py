413c1e68cf6ad98bfe1fa0b3b2319e988056c7cb
Jürgen Eckel <juergen@riddleandcode.com>
2023-05-20 00:31:42 +0200

    blackified
    Signed-off-by: Jürgen Eckel <juergen@riddleandcode.com>

--- x21e8/utils/storage.py
+++ x21e8/utils/storage.py
@@ -11,6 +11,7 @@ ENCODING = "utf-8"
 
 w3s = w3storage.API(token=WEB3STORAGE_TOKEN)
 
+
 def get_ipfs_link(cid: str):
     return "https://" + cid + ".ipfs.w3s.link"
 
@@ -23,28 +24,29 @@ def register_cid_url(cid: str, url: str):
     return cid_resp
 
 
-def decryption_layer( data, decrypt_data: bool = False ):
+def decryption_layer(data, decrypt_data: bool = False):
     if decrypt_data:
         print(f"encrypted : {data}")
         decrypted_blob = decrypt_2_bytes(data)
         print(f"decrypted : {decrypted_blob.decode()}")
         data = decrypted_blob
     else:
-        print(f"data : '{data.decode()}'")    
+        print(f"data : '{data.decode()}'")
 
     return data.decode()
+
 
 def get_ipfs_file(cid: str, decrypt_data: bool = False):
     nft_url = get_ipfs_link(cid)
     nft_data = urlopen(nft_url).read()
-    return decryption_layer( nft_data, decrypt_data)
+    return decryption_layer(nft_data, decrypt_data)
 
 
 def encryption_layer(asset: str, encrypt_data: bool = False):
     if not encrypt_data:
         return asset
     else:
-        return encrypt_bytes(bytes(asset, 'utf-8'))
+        return encrypt_bytes(bytes(asset, "utf-8"))
 
 
 def store_asset(asset: str, encrypt_data: bool = False):
@@ -55,7 +57,7 @@ def store_asset(asset: str, encrypt_data: bool = False):
 
 
 def get_hashed_marshalled(asset: str):
-    asset_bytes=  bytes(asset, 'utf-8')
+    asset_bytes = bytes(asset, "utf-8")
     hashed_marshalled = CID.decode(multihash.digest(asset_bytes, "sha2-256"))
     return hashed_marshalled
 
@@ -70,7 +72,8 @@ def multi_hash(asset: dict, encrypt_data: bool = False):
     hashed_marshalled = get_hashed_marshalled(marshalled_asset)
     return str(hashed_marshalled)
 
-def calculate_cid_v1( asset: str ):
+
+def calculate_cid_v1(asset: str):
     hashed_asset = get_hashed_marshalled(asset)
     cid = get_cid_v1(hashed_asset)
     return str(cid)
